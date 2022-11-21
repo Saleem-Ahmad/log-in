077622e0a1a055a35bfdfdcc16ab02a5969e33bb
Saleem Ahmad <saleemuskan@gmail.com>
2022-11-21 14:00:42 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function registerData() 
+{
+var name= document.getElementById("name").value;
+var email= document.getElementById("email").value;
+var passward= document.getElementById("passward").value;
+console.log(name,email,passward)
+
+
+}
